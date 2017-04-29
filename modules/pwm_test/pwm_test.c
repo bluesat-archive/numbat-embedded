0aa79e317e901d7a70a85b765fa2a612133d8baa
William Miles <wmpmiles@gmail.com>
2017-04-29 16:18:18 +1000

    PWM test code w/ interrupt OWRS-343 #time 4h
    PWM test code output verified with oscilloscope. Setting and changing values
    in real time seems to work.
    
    PWM timer interrupt works but throws an error if it interrupts too quickly. Not sure as to why this is occuring.

--- modules/pwm_test/pwm_test.c
+++ modules/pwm_test/pwm_test.c
@@ -2,32 +2,49 @@
 #include "rtos-kochab.h"
 #include "driverlib/pwm.h"
 
+int counter = 0;
+
 void task_pwm_test_fn(void) {
 
     UARTprintf("Entered PWM Test task. Initializing...\n");
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    GPIOPinConfigure(GPIO_PB6_M0PWM0);
-    GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
-
     SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_6);
+    GPIOPinConfigure(GPIO_PB6_M0PWM0);
 
     PWMClockSet(PWM0_BASE, PWM_SYSCLK_DIV_64);
 
     PWMGenConfigure(PWM0_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN |
                                           PWM_GEN_MODE_NO_SYNC |
-                                          PWM_GEN_MODE_DBG_STOP |
+                                          PWM_GEN_MODE_DBG_RUN |
                                           PWM_GEN_MODE_GEN_NO_SYNC);
 
-    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 0xFFFF);
-    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 0x8FFF);
+    PWMGenPeriodSet(PWM0_BASE, PWM_GEN_0, 50000);
+    PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, 0);
 
     PWMGenEnable(PWM0_BASE, PWM_GEN_0);
     PWMOutputState(PWM0_BASE, PWM_OUT_0_BIT, 1);
 
+    PWMGenIntTrigEnable(PWM0_BASE, PWM_GEN_0, PWM_INT_CNT_LOAD);
+    PWMIntEnable(PWM0_BASE, PWM_INT_GEN_0);
+    IntEnable(26);
+
     UARTprintf("PWM initialisation complete...\n");
     
     while (1);                           
+}
+
+void pwm0_0_isr(void) {
+    counter++;
+
+    if (counter == 156) {
+        counter = 0;
+
+        int width = (PWMPulseWidthGet(PWM0_BASE, PWM_OUT_0) + 12) % 50000;
+        PWMPulseWidthSet(PWM0_BASE, PWM_OUT_0, width);
+    }
 }
 
 int main(void) {
