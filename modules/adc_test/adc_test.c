23b37f1e02c5173c73bcc8181d980fe05edb52d4
William Miles <wmpmiles@gmail.com>
2017-09-16 12:20:25 +1000

    OWRS-342 #time 10m added ADC test module base
    Took pwmlib's test module and created a template for the ADC test
    module.

---
+++ modules/adc_test/adc_test.c
@@ -0,0 +1,34 @@
+#include "boilerplate.h"
+#include "rtos-kochab.h"
+
+void task_adc_test_fn(void) {
+
+    UARTprintf("Entered ADC Test task. Initializing...\n");
+
+    // TEST CODE GOES HERE
+
+    UARTprintf("PWM initialisation complete...\n");
+    
+    while (1);                           
+}
+
+int main(void) {
+
+    // Initialize the floating-point unit.
+    InitializeFPU();
+
+    // Set the clocking to run from the PLL at 50 MHz.
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+
+    // Initialize the UART for stdio so we can use UARTPrintf
+    InitializeUARTStdio();
+
+    // Actually start the RTOS
+    UARTprintf("Starting RTOS...\n");
+    rtos_start();
+
+    /* Should never reach here, but if we do, an infinite loop is
+       easier to debug than returning somewhere random. */
+    for (;;) ;
+}
