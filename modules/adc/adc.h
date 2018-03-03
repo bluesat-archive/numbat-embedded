f22b5dfac8b2f28e5d51b1bd874b59d6e34d56b6
William Miles <wmpmiles@gmail.com>
2018-03-03 13:16:39 +1100

    OWRS-342 #time 30m Modified adc library interface
    Previous interface was for an incorect design born of my
    misunderstanding. The new interface accounts for how one would use it
    within an environment with an RTOS.

--- modules/adc/adc.h
+++ modules/adc/adc.h
@@ -2,10 +2,9 @@
 #define ADC_H
 
 
-// adc types
-
-
-// adc enums
+//------------------------------ENUMS---------------------------------//
+// ADC Pins, using ADC Pin names. See ./PIN_MAPPING for the other names
+// for these pins.
 enum adc_pin {
     AIN0,
     AIN1,
@@ -18,20 +17,36 @@ enum adc_pin {
     AIN20
 };
 
-// adc function status return values
+// Possible return values for the ADC functions.
 enum adc_status {
     ADC_SUCCESS,
     ADC_FAILURE
 };
 
 
-// adc functions
+//----------------------------FUNCTIONS-------------------------------//
+// Initialises the given pins, registers the data buffer, and registers
+// the callback that is called when an ADC capture occurs.
+//
+// The buffer and pins list must have num_pins number of entries.
+//
+// The buffer will be automatically updated on an adc capture. The
+// callback is provided as a way to create a signal of some sort for a 
+// task that will handle the ADC output.
+//
+// This function should ONLY BE RUN ONCE during I/O setup.
+//
+// The maximum number of pins currently supported is 8. This function
+// should fail if num_pins > 8.
+extern enum adc_status adc_init_pins(adc_pin pins[], uint8_t num_pins, 
+    uint16_t buffer[], void (*callback)(void));
 
-// initialises a pin for adc capture
-extern enum adc_status adc_init_pin(adc_pin pin);
-
-// returns the most recently captured value for an 
-// initialised adc pin
-extern enum adc_value adc_get_pin_value(adc_pin pin);
+// Sets the hardware flags that start an ADC capture. 
+//
+// This library currently only supports manual single-shot captures that
+// are started by this function. However, the hardware does support 
+// continuous capture, capture triggered by a PWM module, and capture
+// triggered by an external source.
+extern enum adc_status adc_start_capture();
 
 #endif
