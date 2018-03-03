87dc3e1e14aad554a061642b3512afec21ce9e43
William Miles <wmpmiles@gmail.com>
2018-03-03 14:17:16 +1100

    OWRS-342 #time 1h Updated docs/formatting/layout

--- modules/adc/adc.c
+++ modules/adc/adc.c
@@ -1,83 +1,74 @@
+//------------------------------INCLUDES------------------------------//
 #include "adc.h"
 
 #include <stdbool.h>
 #include <stdint.h>
 
 
-#define PC 9 // number of adc pins
+//---------------------------SIZE_CONSTANTS---------------------------//
+#define PC 9              /* ADC pin count */
+#define GPIO_PORT_COUNT 2 /* number of GPIO ports used by ADC */
 
-// GPIO data structuring
-#define GPIO_PORT_COUNT 2
+
+//-------------------------------ENUMS--------------------------------//
+static enum {
+    PRE_INIT;
+    POST_INIT;
+};
 
 static enum gpio_port {
     GPIOE,
     GPIOD
-};
+}; /* GPIO ports used by ADC */
 
+
+//------------------------------STRUCTS-------------------------------//
 static struct gpio_port {
     uint32_t sysctl;
     uint32_t base;
-};
+}; /* struct for data required for manipulating GPIO ports */
 
 
-// Shared constants, and constant lookup tables
+//---------------------SHARED_HARDWARE_CONSTANTS----------------------//
+/* ADC configuration arguments */
 static const uint32_t adc_clock_div = 1;
 static const uint32_t adc_hw_oversampling = 64;
-static const uint32_t adc_clock_config = ADC_CLOCK_SRC_PIOSC | ADC_CLOCK_RATE_FULL;
+static const uint32_t adc_clock_config = 
+    ADC_CLOCK_SRC_PIOSC | ADC_CLOCK_RATE_FULL;
 
-// Board specific constants, and constant lookup tables
+
+//----------------------BOARD_SPECIFIC_CONSTANTS----------------------//
 #define ADC_TEST_BOARD
-#ifdef ADC_TEST_BOARD
-// Test board constants
-// non-static so that it can be optimised out?
-const struct gpio_port gpio_flags[GPIO_PORT_COUNT] = {
+
+#ifdef ADC_TEST_BOARD /* TEST BOARD */
+static const struct gpio_port gpio_flags[GPIO_PORT_COUNT] = {
     {.sysctl = SYSCTL_PERIPH_GPIOE, .base = GPIO_PORTE_BASE}, 
     {.sysctl = SYSCTL_PERIPH_GPIOB, .base = GPIO_PORTB_BASE}};
     
 static const struct gpio_port gpio_lut[PC] = {
-    gpio_flags[GPIOE], gpio_flags[GPIOE], gpio_flags[GPIOE], gpio_flags[GPIOE],
-    gpio_flags[GPIOB], gpio_flags[GPIOB], gpio_flags[GPIOB], gpio_flags[GPIOB]};
+    gpio_flags[GPIOE], gpio_flags[GPIOE], gpio_flags[GPIOE], 
+    gpio_flags[GPIOE], gpio_flags[GPIOB], gpio_flags[GPIOB], 
+    gpio_flags[GPIOB], gpio_flags[GPIOB]};
 
+#else /* GENERIC PCB */
 
-#else
-// Generic PCB constants
 #endif
 
 
-// Internal functions
-enum adc_status adc_init(void) {
-    // Enable ADC module
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
-
-    // Wait for module to be ready
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))
-        ;
+//------------------------------GLOBALS-------------------------------//
+static int status = PRE_INIT;
+static uint8_t active_pins = 0;
+static uint16_t *buffer = NULL;
+static void (*callback)(void) = NULL;
 
 
-    // Enable ADC clock. Must be 16 MHz, which can be provided by PIOSC
-    ADCClockConfigSet(ADC0_BASE, adc_clock_config, adc_clock_div);
-    // Enable GPIO clock if not enabled
-    // Set GPIO AFSEL bit
-    // Clear GPIO DEN bit
-    // Configure averaging
-    ADCHardwareOversampleConfigure(ADC0_BASE, adc_hw_oversampling);
-    // Register interrupt handler?
+//-----------------------------FUNCTIONS------------------------------//
+enum adc_status adc_init_pins(adc_pin pins[], uint8_t num_pins, 
+    uint16_t buffer[], void (*callback)(void)) {
+        return ADC_FAILURE;
+}
+
+enum adc_status adc_start_capture() {
     return ADC_FAILURE;
 }
-    
 
-// External functions
-
-// initialises a pin for adc capture
-extern enum adc_status adc_init_pin(adc_pin pin) {
-    // Disable isolation via the GPIOAMSEL bit
-    // Disable sample sequencer via ASENn in ADCACTSS
-    // Append pin as source to sequencer via ADCSSCTLn
-    // Re-enable sample sequencer via ASENn in ADCACTSS
-}
-
-// returns the most recently captured value for an 
-// initialised adc pin
-extern enum adc_value adc_get_pin_value(adc_pin pin) {
-    // return the value for the pin from the adc buffer
-}
--- modules/adc/adc.h
+++ modules/adc/adc.h
@@ -1,10 +1,19 @@
+/* Author: William Miles
+ * Date: 2018-03-03
+ *
+ * NB: This library provides a vastly simplified interface to the ADC
+ *     module. If more features or greater performance is required then
+ *     direct usage of the module through the Tiva libraries is 
+ *     recommended. 
+ */
+
 #ifndef ADC_H
 #define ADC_H
 
-
 //------------------------------ENUMS---------------------------------//
-// ADC Pins, using ADC Pin names. See ./PIN_MAPPING for the other names
-// for these pins.
+/* ADC Pins, using ADC Pin names. See ./PIN_MAPPING for the other names
+ * for these pins.
+ */
 enum adc_pin {
     AIN0,
     AIN1,
@@ -17,36 +26,37 @@ enum adc_pin {
     AIN20
 };
 
-// Possible return values for the ADC functions.
+/* Possible return values for the ADC functions. */
 enum adc_status {
     ADC_SUCCESS,
     ADC_FAILURE
 };
 
-
 //----------------------------FUNCTIONS-------------------------------//
-// Initialises the given pins, registers the data buffer, and registers
-// the callback that is called when an ADC capture occurs.
-//
-// The buffer and pins list must have num_pins number of entries.
-//
-// The buffer will be automatically updated on an adc capture. The
-// callback is provided as a way to create a signal of some sort for a 
-// task that will handle the ADC output.
-//
-// This function should ONLY BE RUN ONCE during I/O setup.
-//
-// The maximum number of pins currently supported is 8. This function
-// should fail if num_pins > 8.
+/* Initialises the given pins, registers the data buffer, and registers
+ * the callback that is called when an ADC capture occurs.
+ *
+ * The buffer and pins list must have num_pins number of entries.
+ *
+ * The buffer will be automatically updated on an adc capture. The
+ * callback is provided as a way to create a signal of some sort for a 
+ * task that will handle the ADC output.
+ *
+ * This function should ONLY BE RUN ONCE during I/O setup.
+ *
+ * The maximum number of pins currently supported is 8. This function
+ * should fail if num_pins > 8.
+ */
 extern enum adc_status adc_init_pins(adc_pin pins[], uint8_t num_pins, 
     uint16_t buffer[], void (*callback)(void));
 
-// Sets the hardware flags that start an ADC capture. 
-//
-// This library currently only supports manual single-shot captures that
-// are started by this function. However, the hardware does support 
-// continuous capture, capture triggered by a PWM module, and capture
-// triggered by an external source.
+/* Sets the hardware flags that start an ADC capture. 
+ *
+ * This library currently only supports manual single-shot captures that
+ * are started by this function. However, the hardware does support 
+ * continuous capture, capture triggered by a PWM module, and capture
+ * triggered by an external source.
+ */
 extern enum adc_status adc_start_capture();
 
 #endif
