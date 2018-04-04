9dd34ddda2dd911e46fe43abf59f14bee9f62664
Elliott Smith <cbrakespear@hotmail.com.au>
2018-04-04 11:27:09 +1000

    Adding files

---
+++ modules/left_locomotion/mod_left_locomotion.c
@@ -0,0 +1,24 @@
+#include "pwm.h"
+#include "boilerplate.h"
+#include "rtos-kochab.h"
+
+
+int main(void) {
+
+    // Initialize the floating-point unit.
+    InitializeFPU();
+
+    // Set the clocking to run from the PLL at 50 MHz.
+    ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+    // Initialize the UART for stdio so we can use UARTPrintf
+    InitializeUARTStdio();
+    
+    // Actually start the RTOS
+    UARTprintf("Starting RTOS...\n");
+    rtos_start();
+
+    /* Should never reach here, but if we do, an infinite loop is
+       easier to debug than returning somewhere random. */
+    for (;;) ;
+}
