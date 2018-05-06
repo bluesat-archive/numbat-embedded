c1f05936d5cd478a00af914c887deb492e56cb6a
hjed <harry@dayfamilyweb.com>
2018-05-06 14:47:59 +1000

    ROC-2 #time 30m made the ros echronos build script make more sense

--- build-tools/construct_numbat_module.cmake
+++ build-tools/construct_numbat_module.cmake
@@ -4,5 +4,4 @@ set(PRJ_TOOL enchronos/bin/prj.sh)
 
 function(construct_numbat_module module_dir module_name)
     build_echronos_module(${module_name})
-    build_ros_echronos(${module_dir} ${module_name} ${module_name}-echronos.a 0 1)
 endfunction()
--- src/libs/ros_echronos/ros_echronos.cmake
+++ src/libs/ros_echronos/ros_echronos.cmake
@@ -10,29 +10,33 @@ set(CMAKE_VERBOSE_MAKEFILE 1)
 
 set(ROS_ECHRONOS_DIR ../../libs/ros_echronos/)
 set(ROS_ARCH_DIR ${ROS_ECHRONOS_DIR}/arch/tm4c)
+set(ROS_INCLUDE_DIR ${ROS_ECHRONOS_DIR}/include)
 
 FILE(GLOB ros_files ${ROS_ECHRONOS_DIR}/arch/tm4c/*.cpp ${ROS_ECHRONOS_DIR}/*.cpp )
-
+FILE(GLOB ros_include_files ${ROS_INCLUDE_DIR}/*.hpp ${ROS_INCLUDE_DIR}/templates/*.hpp )
 
 
 
 function(build_ros_echronos echronos_build_dir module_name echronos_target node_id serial_on)
     set(ROS_BUILD_DIR ${echronos_build_dir}/lib/ros-echronos)
-    add_custom_command(
-            OUTPUT ${ROS_BUILD_DIR}
-            COMMAND mkdir -p ${ROS_BUILD_DIR}
-    )
 #    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -Dgcc -DROS_NODE_ID=${node_id} -DROS_INFO_SERIAL=${serial_on}")
     message(STATUS "ROS FILES ${ros_files}")
-    add_library(ros_echronos_${module_name} STATIC ${ros_files})
+    target_sources(${module_name}
+        PRIVATE
+            ${ros_files}
+        PUBLIC
+            ${ros_include_files}
+    )
+#    add_library(ros_echronos_${module_name} STATIC ${ros_files})
     target_include_directories(
-            ros_echronos_${module_name} PUBLIC
+            ${module_name} PUBLIC
             ${ROS_ECHRONOS_DIR}/include
             ${LIB_DIR}/tlsf #TODO: make this its own file
             ${echronos_build_dir}
             ${CMAKE_INCLUDE_PATH}
     )
-    set_target_properties(ros_echronos_${module_name} PROPERTIES COMPILE_FLAGS "-DROS_NODE_ID=${node_id} -DROS_INFO_SERIAL=${serial_on}")
-    add_dependencies(ros_echronos_${module_name} ${echronos_target})
+    set_target_properties(${module_name} PROPERTIES COMPILE_FLAGS "-DROS_NODE_ID=${node_id} -DROS_INFO_SERIAL=${serial_on}")
+    add_dependencies(${module_name} ${echronos_target})
+    target_link_libraries(${module_name} driverlib utils tlsf)
 #    add_dependencies(ros_echronos_${module_name} ${ROS_BUILD_DIR})
 endfunction()
