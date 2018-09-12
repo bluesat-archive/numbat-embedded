3416e3c73ce695e8b04b330150c85f5c367b6582
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-09-12 19:10:43 +0200

    Whitespace cleanup

--- src/libs/i2c/science-mod/TCA9548A.cpp
+++ src/libs/i2c/science-mod/TCA9548A.cpp
@@ -3,7 +3,7 @@
 #define TCA_ADDRESS 0x70
 
 void i2c_select(i2cModule_t module, uint8_t num) {
-    i2c_set_slave_addr(module, TCA_ADDRESS, false);    
+    i2c_set_slave_addr(module, TCA_ADDRESS, false);
     i2c_write(module, 1 << num, I2C_CMD_SINGLE_SEND);
 }
 
